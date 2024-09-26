1507ee7556a1067581a4e0a962d4eebaaeb1b678
Ismael <ismaelguedessilva@gmail.com>
2024-09-26 11:19:36 -0300

    first commit

---
+++ drupal-docker/sites/default/files/js/js_gqflmHmcLD_Jwd0i9v5SKjz4FxsjH9DKABTBbQw6-Ls.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * A replacement for Modernizr touch events detection.
+ */
+
+document.documentElement.classList.add(
+  'ontouchstart' in window ||
+    (window.DocumentTouch && document instanceof window.DocumentTouch)
+    ? 'touchevents'
+    : 'no-touchevents',
+);
+;
+;
